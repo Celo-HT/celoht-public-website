a4fc22876138d8a809253b0b4458f0141866dea9
CeloHT <celoht3@gmail.com>
2025-12-25 20:53:35 -0400

    Crear app/page.tsx

---
+++ app/app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <main className="space-y-20">
+      <section className="hero">
+        <h1>Lajan Digital. Edikasyon Blockchain. Avni Ayiti.</h1>
+      </section>
+    </main>
+  );
+}
